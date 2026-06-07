373f23f124a3a5eb48638187ecc3f11494dfba88
Sidhesh <116739966+sidhesh-22@users.noreply.github.com>
2026-06-07 11:32:18 +0100

    Update ddl_bronze.sql
    Added DDL scripts

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,59 @@
+/*
+=============================================================================================
+DDL Script: Create Bronze Layer Tables
+=============================================================================================
+Script Purpose:
+  This script creates tables in the 'Bronze' schema, dropping existing tables if they exist.
+  Run this script to re-define the DDL structure of 'bronze' tables
+=============================================================================================
+*/
 
+create table if not exists bronze.crm_cust_info (
+	cst_id int,
+	cst_key varchar(50),
+	cst_firstname varchar(50),
+	cst_lastname varchar(50),
+	cst_marital_status varchar(50),
+	cst_gender varchar(50),
+	cst_create_date date
+);
+
+create table if not exists bronze.crm_prd_info (
+	prd_id int,
+	prd_key varchar(50),
+	prd_nm varchar(50),
+	prd_cost int,
+	prd_line varchar(50),
+	prd_start_dt date,
+	prd_end_dt date
+);
+
+create table if not exists bronze.crm_sales_details (
+	sls_ord_num varchar(50),
+	sls_prd_key varchar(50),
+	sls_cust_id int,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int
+);
+
+create table if not exists bronze.erp_cust_az12 (
+	cid varchar(50),
+	bdate date,
+	gen varchar(50)
+);
+
+create table if not exists bronze.erp_loc_a101 (
+	cid varchar(50),
+	cntry varchar(50)
+);
+
+create table if not exists bronze.erp_px_cat_g1v2 (
+	id varchar(50),
+	cat varchar(50),
+	subcat varchar(50),
+	maintenance varchar(50)
+);
